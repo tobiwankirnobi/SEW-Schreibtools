80f4a598ecbc09ca60e1211f9e3e439c4db67d0e
Tobias Kirnberger <tobias@kirnberger.at>
2020-10-14 12:11:09 +0200

    test

---
+++ Schreibtools/Schreibtools/Stift.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schreibtools
+{
+    class Stift
+    {
+        public double Strichstaerker { get; set; }
+        public ConsoleColor Farbe { get; set; }
+        public string Typ { get; set; }
+
+    }
+}
